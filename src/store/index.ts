40e12c2b7edc2c354fb59e05a289a88abc520de2
LuanAnders0n <luan.anderson.001@gmail.com>
2022-10-10 20:58:00 -0300

    inicialization-redux

---
+++ src/store/index.ts
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+const INITIAL_STATE = {
+  comics: []
+}
+
+function reducer(state = INITIAL_STATE, action: AnyAction) {
+  switch (action.type){
+    case  'GET_COMICS_LIST'[
+
+    ]
+  }
+  return []
+}
+
+const store = configureStore({reducer:{
+
+}
+})
+
+export default store
